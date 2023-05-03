aa1970174a4ea56f77fa975080bc71dc7a1873a8
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2023-05-03 10:28:26 +0300

    Add files via upload

---
+++ lab04/lab0401/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+public class Planet
+{
+    private string name;
+    private int continentsCount;
+
+    public Planet(string name)
+    {
+        this.name = name;
+    }
+
+    public void AddContinent(Continent continent)
+    {
+        continentsCount++;
+        Console.WriteLine($"Added continent {continent.Name} to planet {name}");
+    }
+
+    public void PrintContinents()
+    {
+        Console.WriteLine($"Planet {name} has {continentsCount} continents:");
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Planet planet = (Planet)obj;
+        return name.Equals(planet.name) && continentsCount == planet.continentsCount;
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode() ^ continentsCount.GetHashCode();
+    }
+}
+
+public class Continent
+{
+    private string name;
+
+    public Continent(string name)
+    {
+        this.name = name;
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Continent continent = (Continent)obj;
+        return name.Equals(continent.name);
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode();
+    }
+}
+
+public class Ocean
+{
+    private string name;
+
+    public Ocean(string name)
+    {
+        this.name = name;
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Ocean ocean = (Ocean)obj;
+        return name.Equals(ocean.name);
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode();
+    }
+}
+
+public class Island
+{
+    private string name;
+
+    public Island(string name)
+    {
+        this.name = name;
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public override string ToString()
+    {
+        return name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Island island = (Island)obj;
+        return name.Equals(island.name);
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Planet earth = new Planet("Earth");
+
+        Continent asia = new Continent("Asia");
+        earth.AddContinent(asia);
+
+        Continent europe = new Continent("Europe");
+        earth.AddContinent(europe);
+
+        Ocean pacific = new Ocean("Pacific");
+        Island hawaii = new Island("Hawaii");
+
+        Console.WriteLine($"Created {earth}, {asia}, {europe}, {pacific}, {hawaii}");
+        earth.PrintContinents();
+    }
+}
---
+++ lab04/lab0402/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace Quadrangles
+{
+    class TQuadrangle
+    {
+        protected double a, b, c, d;
+
+        public TQuadrangle(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public virtual double CalculateArea()
+        {
+            return 0;
+        }
+
+        public virtual double CalculatePerimeter()
+        {
+            return 0;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("Quadrangle: a={0}, b={1}, c={2}, d={3}", a, b, c, d);
+        }
+    }
+
+    class Rectangle : TQuadrangle
+    {
+        public Rectangle(double a, double b) : base(a, b, a, b)
+        {
+        }
+
+        public override double CalculateArea()
+        {
+            return a * b;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (a + b);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Rectangle: a={0}, b={1}", a, b);
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public Square(double a) : base(a, a)
+        {
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Square: a={0}", a);
+        }
+    }
+
+    class Parallelogram : TQuadrangle
+    {
+        private double h;
+
+        public Parallelogram(double a, double b, double h) : base(a, b, a, b)
+        {
+            this.h = h;
+        }
+
+        public override double CalculateArea()
+        {
+            return a * h;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (a + b);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Parallelogram: a={0}, b={1}, h={2}", a, b, h);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n = 10;
+            TQuadrangle[] quadrangles = new TQuadrangle[n];
+
+            // Create n quadrangles: 4 rectangles, 4 squares, and 2 parallelograms
+            for (int i = 0; i < n; i++)
+            {
+                switch (random.Next(3))
+                {
+                    case 0:
+                        quadrangles[i] = new Rectangle(random.Next(1, 11), random.Next(1, 11));
+                        break;
+                    case 1:
+                        quadrangles[i] = new Square(random.Next(1, 11));
+                        break;
+                    case 2:
+                        quadrangles[i] = new Parallelogram(random.Next(1, 11), random.Next(1, 11), random.Next(1, 11));
+                        break;
+                }
+            }
+
+            double totalRectangleArea = 0;
+            double totalSquareArea = 0;
+            double totalParallelogramPerimeter = 0;
+
+            foreach (TQuadrangle quadrangle in quadrangles)
+            {
+                quadrangle.Display();
+                if (quadrangle is Rectangle)
+                {
+                    totalRectangleArea += quadrangle.CalculateArea();
+                }
+                else if (quadrangle is Square)
+                {
+                    totalSquareArea += quadrangle.CalculateArea();
+                }
+                else if (quadrangle is Parallelogram)
+                {
+                    totalParallelogramPerimeter += quadrangle.CalculatePerimeter();
+                }
+            }
+
+            Console.WriteLine("Total area of rectangles: {0}", totalRectangleArea);
+            Console.WriteLine("Total area of squares: {0}", totalSquareArea);
+            Console.WriteLine("Total perimeter of parallelograms: {0}", totalParallelogramPerimeter);
+
+            Console.ReadLine();
+        }
+    }
+}
+
